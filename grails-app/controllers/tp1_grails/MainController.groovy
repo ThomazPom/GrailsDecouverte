51da4e5676e54259b297c9ad35e48a50bc7d3018
Thomas Benhamou <Thomas Benhamou>
2016-09-23 14:23:18 +0200

    Navbar

--- grails-app/controllers/tp1_grails/MainController.groovy
+++ grails-app/controllers/tp1_grails/MainController.groovy
@@ -5,7 +5,7 @@ class MainController {
 
     def index() {
 
-        def htmlContent = "<h1>HelloWorld</h1>"
-        render text: htmlContent, contentType:"text/html", encoding:"UTF-8"
+        render(view: "index");
+
     }
 }
